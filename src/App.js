9ed2d07dce6e1a813c6f5b64ba81f8d08b960724
Samantha Deschaepmeester <deschaepmeester.samantha@gmail.com>
2021-03-20 18:31:58 +0100

    Add styling to QR Code / bar Code Scanner

--- src/App.js
+++ src/App.js
@@ -125,14 +125,18 @@ export default class App extends Component{
             justifyContent: 'center',
             alignItems: 'center',
         }}>
-          <BarCodeScanner
-          onBarCodeScanned = {this.barCodeScanned }
-          style = {{
-              height:  DEVICE_HEIGHT/1.1,
-              width: DEVICE_WIDTH,
-          }}
-          >
-          </BarCodeScanner>
+     <BarCodeScanner
+        onBarCodeScanned = {this.barCodeScanned }
+        style={[StyleSheet.absoluteFill, styles.container]}
+      >
+        <View style={styles.layerTop} />
+        <View style={styles.layerCenter}>
+          <View style={styles.layerLeft} />
+          <View style={styles.focused} />
+          <View style={styles.layerRight} />
+        </View>
+        <View style={styles.layerBottom} />
+      </BarCodeScanner>
         </View>
       );
       
@@ -140,11 +144,42 @@ export default class App extends Component{
   }
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+}); */
+
+const opacity = 'rgba(0, 0, 0, .6)';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  layerTop: {
+    flex: 2,
+    backgroundColor: opacity
+  },
+  layerCenter: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  layerLeft: {
+    flex: 1,
+    backgroundColor: opacity
+  },
+  focused: {
+    flex: 10
+  },
+  layerRight: {
+    flex: 1,
+    backgroundColor: opacity
+  },
+  layerBottom: {
+    flex: 2,
+    backgroundColor: opacity
   },
 });
