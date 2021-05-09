9bc4f98fe78c9861f2858fa3daa6e176e9964d62
Thibaud <Thibaud.roussel@edu.itescia.fr>
2021-05-09 22:59:52 +0200

    No auth system

--- src/App.js
+++ src/App.js
@@ -27,15 +27,15 @@ const App = ({navigation}) => {
         detectLogin()
     },[])
 
+   /* <Stack.Screen name="login" component={SigninScreen} />
+    <Stack.Screen name="signup" component={SignupScreen} />
+    <Stack.Screen name="loading" component={LoadingScreen} />*/
 
 
     return(
         <NavigationContainer  >
             <Stack.Navigator headerMode={"none"}>
-                <Stack.Screen name="login" component={SigninScreen} />
-                <Stack.Screen name="signup" component={SignupScreen} />
-                <Stack.Screen name="loading" component={LoadingScreen} />
-                <Stack.Screen name={"tab"} children={()=> <Tabs/>}/>
+               <Stack.Screen name={"tab"} children={()=> <Tabs/>}/>
                 <Stack.Screen name="drugs" component={DrugsScreen} />
             </Stack.Navigator>
         </NavigationContainer>
