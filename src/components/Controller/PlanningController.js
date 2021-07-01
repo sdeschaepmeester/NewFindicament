2706b35f062b4395fdc03610dca78127efa7ce37
Deschaepmeester Samantha <deschaepmeester.samantha@gmail.com>
2021-07-01 15:43:39 +0200

    Fix structure (add class instead of multiple functions)

--- src/components/Controller/PlanningController.js
+++ src/components/Controller/PlanningController.js
@@ -1,35 +1,12 @@
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import React, { useState } from "react";
-import {
-  ViewStyle,
-  TextStyle,
-  TextInputProps,
-  SafeAreaView,
-  ScrollView,
-  CheckBox,
-  ImageBackground,
-  View,
-  FlatList,
-  Alert,
-  StyleSheet,
-  Text,
-  TextBase,
-  TextInput,
-  TextInputBase,
-  Switch,
-  selectedDotColor,
-
-} from 'react-native';
+import React, { useState, Component } from "react";
+import { ViewStyle, TextStyle, TextInputProps, SafeAreaView, ScrollView, CheckBox, ImageBackground, View, FlatList, Alert, StyleSheet, Text, TextBase, TextInput, TextInputBase, Switch, selectedDotColor, } from 'react-native';
 import { List, Button, Avatar, Checkbox } from 'react-native-paper';
 import { LocaleConfig } from 'react-native-calendars';
-import { enableExpoCliLogging } from 'expo/build/logs/Logs';
-import { useForm } from "react-hook-form";
-import ReactDOM from "react-dom";
 import { Card, ListItem, Icon } from 'react-native-elements';
 import { insertToPlanning } from '../goToPlanning';
-
 
 LocaleConfig.locales['fr'] = {
   monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
@@ -40,110 +17,143 @@ LocaleConfig.locales['fr'] = {
 };
 LocaleConfig.defaultLocale = 'fr';
 
-const Stack = createStackNavigator();
-
 function AddTreatment({ navigation, day }) {
-  navigation.navigate('Treatment',{day})
+  navigation.navigate('Treatment', { day })
   console.log("Voici le futur start_date:")
   console.log(day)
 }
 
-function PlanningScreen({ navigation }) {
-  return (
-    <View>
-      <CalendarList
-        // Callback which gets executed when visible months change in scroll view. Default = undefined
-        onVisibleMonthsChange={(months) => { console.log('now these months are visible', months); }}
-        // Max amount of months allowed to scroll to the past. Default = 50
-        pastScrollRange={50}
-        // Max amount of months allowed to scroll to the future. Default = 50
-        futureScrollRange={50}
-        // Enable or disable scrolling of calendar list
-        scrollEnabled={true}
-        // Enable or disable vertical scroll indicator. Default = false
-        showScrollIndicator={true}
-        //...calendarParams
-        /* onDayPress={(day)=>{selectedDayBackgroundColor='#60d2e4'}}
-        onDayPress={(day)=>{day.selectedDotColor='#60d2e4'}}*/
-        onDayPress={(day) => AddTreatment({ navigation, day })}
-        markedDates={{
+class Planning extends Component {
 
-          '2021-05-22': { startingDay: true, color: '#00adf5' },
-          '2021-05-23': { startingDay: false, color: '#00adf5' },
-          '2021-05-24': { startingDay: false, color: '#00adf5' },
-          '2021-05-25': { startingDay: false, color: '#00adf5' },
-          '2021-05-26': { endingDay: true, color: '#00adf5' },
-        }}
-        // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
-        markingType={'period'}
-        //onDayPress={()=>navigation.navigate('Details')}
-        // Callback that gets called when day changes while scrolling agenda list
-        style={{
-          borderWidth: 1,
-          borderColor: 'gray',
+  state = {
+    plannings: [
+    ]
+  };
 
-        }}
-        // Specify theme properties to override specific styles for calendar parts. Default = {}
-        theme={{
-          backgroundColor: '#ffffff',
-          calendarBackground: '#ffffff',
-          textSectionTitleColor: '#b6c1cd',
-          textSectionTitleDisabledColor: '#d9e1e8',
-          selectedDayBackgroundColor: '#60d2e4',
-          selectedDayTextColor: '#ffffff',
-          todayTextColor: '#00adf5',
-          dayTextColor: '#2d4150',
-          textDisabledColor: '#d9e1e8',
-          dotColor: '#00adf5',
-          selectedDotColor: '#ffffff',
-          arrowColor: 'orange',
-          disabledArrowColor: '#d9e1e8',
-          monthTextColor: '#60d2e4',
-          indicatorColor: 'blue',
-          textDayFontWeight: '300',
-          textMonthFontWeight: 'bold',
-          textDayHeaderFontWeight: '300',
-          textDayFontSize: 16,
-          textMonthFontSize: 16,
-          textDayHeaderFontSize: 16
-        }}
-      />
-    </View>
-  )
-};
+  async getPlanning() {
+    // Get planning
+    const planningsResponses = await fetch('http://10.0.2.2:3000/getPlanning', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })
+      .then(res => res.json())
+      .then((responseJson) => {
+        return responseJson;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    const plannings = await planningsResponses
+    this.setState({ plannings: plannings })
+  } // Get planning 
+
+  async componentDidMount() {
+    console.log("GET PLANNING")
+    await this.getPlanning()
+    this.focusListener = this.props.navigation.addListener("focus", async () => {
+      console.log("start 2")
+      await this.getPlanning()
+
+    });
+  } // ComponentDidMount
+
+
+  render() {
+
+    let plannings = this.state.plannings;
+    const { navigation } = this.props;
 
 
 
+    return (
+      <View>
+        <CalendarList
+          // Callback which gets executed when visible months change in scroll view. Default = undefined
+          onVisibleMonthsChange={(months) => { console.log('now these months are visible', months); }}
+          // Max amount of months allowed to scroll to the past. Default = 50
+          pastScrollRange={50}
+          // Max amount of months allowed to scroll to the future. Default = 50
+          futureScrollRange={50}
+          // Enable or disable scrolling of calendar list
+          scrollEnabled={true}
+          // Enable or disable vertical scroll indicator. Default = false
+          showScrollIndicator={true}
+          //...calendarParams
+          /* onDayPress={(day)=>{selectedDayBackgroundColor='#60d2e4'}}
+          onDayPress={(day)=>{day.selectedDotColor='#60d2e4'}}*/
+          onDayPress={(day) => AddTreatment({ navigation, day })}
+          markedDates={{
 
-const stylesAddTreatment = {
-  button: {
-    backgroundColor: 'blue',
-  },
-  backgroundColor: 'lightblue'
-}
+
+            // '2021-05-22': { startingDay: true, color: '#00adf5' },
+            // '2021-05-23': { startingDay: false, color: '#00adf5' },
+            // '2021-05-24': { startingDay: false, color: '#00adf5' },
+            // '2021-05-25': { startingDay: false, color: '#00adf5' },
+            // '2021-05-26': { endingDay: true, color: '#00adf5' },
+          }}
+          // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
+          markingType={'period'}
+          //onDayPress={()=>navigation.navigate('Details')}
+          // Callback that gets called when day changes while scrolling agenda list
+          style={{
+            borderWidth: 1,
+            borderColor: 'gray',
+
+          }}
+          // Specify theme properties to override specific styles for calendar parts. Default = {}
+          theme={{
+            backgroundColor: '#ffffff',
+            calendarBackground: '#ffffff',
+            textSectionTitleColor: '#b6c1cd',
+            textSectionTitleDisabledColor: '#d9e1e8',
+            selectedDayBackgroundColor: '#60d2e4',
+            selectedDayTextColor: '#ffffff',
+            todayTextColor: '#00adf5',
+            dayTextColor: '#2d4150',
+            textDisabledColor: '#d9e1e8',
+            dotColor: '#00adf5',
+            selectedDotColor: '#ffffff',
+            arrowColor: 'orange',
+            disabledArrowColor: '#d9e1e8',
+            monthTextColor: '#60d2e4',
+            indicatorColor: 'blue',
+            textDayFontWeight: '300',
+            textMonthFontWeight: 'bold',
+            textDayHeaderFontWeight: '300',
+            textDayFontSize: 16,
+            textMonthFontSize: 16,
+            textDayHeaderFontSize: 16
+          }}
+        />
+      </View >
+    )
+  } // render 
+}; //planning
 
 function AddTreatmentScreen({ route, navigation }) {
   const { day } = route.params;
-  const futureStartDate = JSON.stringify(day.day)+"/"+JSON.stringify(day.month)+"/"+JSON.stringify(day.year);
+  const futureStartDate = JSON.stringify(day.day) + "/" + JSON.stringify(day.month) + "/" + JSON.stringify(day.year);
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [isSelected, setSelection] = useState(false);
   const [inputName, onChangeInputName] = React.useState();
   const [inputComment, onChangeInputComment] = React.useState();
 
-  let AddTreatmentPlanning = ({navigation},name, comment, start_date, day) => {
-    console.log("frkgopkdfgjdfklxjgkldf")
-    console.log(toString(day.day))
+  let AddTreatmentPlanning = ({ navigation }, name, comment, start_date, day) => {
     navigation.navigate('Planning')
-    insertToPlanning(name,comment, start_date)
+    insertToPlanning(name, comment, start_date)
   }
 
   return (
     <View style={styles.container}>
       <Card>
         <Text style={{ fontSize: 30 }}>Ajouter un traitement</Text>
-        <Text style={{marginLeft: 18, marginRight: 18, marginTop: 20,marginBottom: 20}}>Début du traitement le {JSON.stringify(day.day)}/{JSON.stringify(day.month)}/{JSON.stringify(day.year)} </Text>
-        
+        <Text style={{ marginLeft: 18, marginRight: 18, marginTop: 20, marginBottom: 20 }}>Début du traitement le {JSON.stringify(day.day)}/{JSON.stringify(day.month)}/{JSON.stringify(day.year)} </Text>
+
         <TextInput
           style={{ height: 40, borderRadius: 2, borderWidth: 1, borderColor: 2374 }}
           placeholder="Sélectionner le médicament"
@@ -159,29 +169,20 @@ function AddTreatmentScreen({ route, navigation }) {
           <Text style={styles.label}>Traitement régulier</Text>
         </View>
         <TextInput
-            style={{  borderRadius: 2, borderWidth: 1, borderColor: 2374 }}
-            placeholder="Entrer un commentaire"
-            multiline={true}
-            numberOfLines={3}
-            onChangeText={onChangeInputComment}
-            value={inputComment}
-          />
+          style={{ borderRadius: 2, borderWidth: 1, borderColor: 2374 }}
+          placeholder="Entrer un commentaire"
+          multiline={true}
+          numberOfLines={3}
+          onChangeText={onChangeInputComment}
+          value={inputComment}
+        />
 
 
-
-
-
-
-
-
-
-
-
-        <Button onPress={() => AddTreatmentPlanning({navigation}, inputName, inputComment, futureStartDate, day)}  style={{ backgroundColor: "#0099ff", marginLeft: 18, marginRight: 18, marginTop: 20}}>
+        <Button onPress={() => AddTreatmentPlanning({ navigation }, inputName, inputComment, futureStartDate, day)} style={{ backgroundColor: "#0099ff", marginLeft: 18, marginRight: 18, marginTop: 20 }}>
           Confirmer
         </Button>
-            
-        <Button style={{ backgroundColor: "#0099ff", marginLeft: 18, marginRight: 18, marginTop: 20}}>
+
+        <Button style={{ backgroundColor: "#0099ff", marginLeft: 18, marginRight: 18, marginTop: 20 }}>
           Annuler
         </Button>
       </Card>
@@ -190,16 +191,30 @@ function AddTreatmentScreen({ route, navigation }) {
 };
 
 
-const DrugsScreen = ({ navigation }) => {
-  return (
-    <NavigationContainer independent={true}>
-      <Stack.Navigator initialRouteName="Planning" >
-        <Stack.Screen name="Planning" component={PlanningScreen} />
-        <Stack.Screen name="Treatment" component={AddTreatmentScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+
+
+
+
+
+
+
+
+export { Planning as PlanningClass }
+
+export default class PlanningStack extends Component {
+  render() {
+    const Stack = createStackNavigator();
+    return (
+      <NavigationContainer independent={true}>
+        <Stack.Navigator initialRouteName="Planning" >
+          <Stack.Screen name="Planning" component={Planning} />
+          <Stack.Screen name="Treatment" component={AddTreatmentScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -220,4 +235,6 @@ const styles = StyleSheet.create({
   },
 });
 
-export default DrugsScreen;
+
+
+
